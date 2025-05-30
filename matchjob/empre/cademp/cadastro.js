a250ae1d78f5971893b80601c47aaa9062141992
Gustavo <gustavorlazaro@gmail.com>
2025-05-30 11:53:58 -0300

    Ajustando img e add conf de senha forte

--- matchjob/empre/cademp/cadastro.js
+++ matchjob/empre/cademp/cadastro.js
@@ -1,17 +1,23 @@
 function cadastrar() {
-    const nome = document.getElementById('nome').value;
-    const email = document.getElementById('email').value;
     const senha = document.getElementById('senha').value;
     const senha2 = document.getElementById('senha2').value;
+    const mensagemErro = document.getElementById('mensagemErro');
 
-    if (senha !== senha2) {
-        document.getElementById('mensagemErro').innerText = "As senhas não coincidem!";
+    const regexSenha = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\W).{8,}$/;
+
+    if (!regexSenha.test(senha)) {
+        mensagemErro.textContent = 'A senha deve ter pelo menos 8 caracteres, incluindo letra maiúscula, minúscula e caractere especial.';
         return;
     }
 
-    // Armazenar nome no localStorage
+    if (senha !== senha2) {
+        mensagemErro.textContent = 'As senhas não coincidem.';
+        return;
+    }
+
+// Armazenar nome no localStorage
     localStorage.setItem('nomeUsuario', nome);
 
     // Redirecionar para a página inicial
-    window.location.href = "../iniemp/empre.html";
+    window.location.href = "../inicio/inicio.html";
 }
